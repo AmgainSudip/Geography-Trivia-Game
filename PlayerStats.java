6fd7060bea1300fce08b9517bc69ac62614ff04a
Sudip Amgain <sudipamgain2061@gmail.com>
2025-03-31 21:29:07 -0400

    PlayerStats.java

--- PlayerStats.java
+++ PlayerStats.java
@@ -1,32 +1,38 @@
 package trivia.game;
 
-public class PlayerStats { //Handle Player Score and Stats
-	int playerScore = 0;
-	int playerLives = 3;
-	int questionCount = 0;
-	
-	public void updatePlayerScore() {
-		playerScore += 10; 
-	}
-	
-	public void updatePlayerLives() {
-		playerLives -= 1;
-	}
-	
-	public void updateQuestionCount() {
-		questionCount += 1;
-	}
-	
-	public int getPlayerScore() {
-		return playerScore; 
-	}
-	
-	public int getPlayerLives() {
-		return playerLives;
-	}
-	
-	public int getQuestionCount() {
-		return questionCount;
-	}
-	
+public class PlayerStats { // Handles Player Score and Stats
+    int playerScore = 0;  // Player's score, initialized to 0
+    int playerLives = 3;  // Player's lives, initialized to 3
+    int questionCount = 0;  // Number of questions answered, initialized to 0
+
+    // Method to update the player's score by 10 for each correct answer
+    public void updatePlayerScore() {
+        playerScore += 10;  // Increase score by 10
+    }
+
+    // Method to update the player's lives by subtracting 1 for each incorrect answer
+    public void updatePlayerLives() {
+        playerLives -= 1;  // Decrease lives by 1
+    }
+
+    // Method to update the question count every time a question is answered
+    public void updateQuestionCount() {
+        questionCount += 1;  // Increase question count by 1
+    }
+
+    // Getter method to return the player's current score
+    public int getPlayerScore() {
+        return playerScore;  // Return the player's score
+    }
+
+    // Getter method to return the player's remaining lives
+    public int getPlayerLives() {
+        return playerLives;  // Return the player's lives
+    }
+
+    // Getter method to return the number of questions answered
+    public int getQuestionCount() {
+        return questionCount;  // Return the question count
+    }
 }
+
